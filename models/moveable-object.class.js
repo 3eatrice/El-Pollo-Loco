b2b7b4994ca183a0c317dbfd91283d5493b95cb8
Beatrice Ambos <beatrice.ambos@web.de>
2023-02-20 12:10:41 +0100

    first commit

---
+++ models/moveable-object.class.js
@@ -0,0 +1,20 @@
+class MoveableObject {
+    x = 120;
+    y = 200;
+    img;
+    height =  100;
+    width =  100;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('Moving Right')
+    }
+
+    moveLeft() {
+        
+    }
+}
